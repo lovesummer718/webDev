da9a7de0c456414cdead84ffbf8bd33e43b5a0c3
Wang Xujun <lovesummer718@gmail.com>
2023-04-21 16:57:00 +0800

    First update

---
+++ index.js
@@ -0,0 +1,51 @@
+// console.log("Hello, world!");
+
+// let age = 24;
+// isAdult = 18 - age;
+
+// if (isAdult >= 0) {
+//     console.log("You are not an adult.");
+// } else {
+//     console.log("You are an adult.");
+// }
+// const students = {
+//     "name": "Simon",
+//     "name2": "John"
+// }
+
+// console.log(students.name);
+
+/*
+console.log(Math.random());
+console.log(Math.floor(Math.random() * 100));
+ */
+
+/* let language = "JavaScript";
+let message = `Let's learn ${language}`;
+console.log(message);
+ */
+
+// let str3 = "New \nline.";
+// let str4 = "I'm containing a backslash: \\!";
+// console.log(str3);
+// console.log(str4);
+
+// let str = "Hello";
+// let nr = 7;
+// let bigNr = 12345678901234n;
+// let bool = true;
+// let sym = Symbol("unique");
+// let undef = undefined;
+// let unknown = null;
+// console.log("str", typeof str);
+// console.log("nr", typeof nr);
+// console.log("bigNr", typeof bigNr);
+// console.log("bool", typeof bool);
+// console.log("sym", typeof sym);
+// console.log("undef", typeof undef);
+// console.log("unknown", typeof unknown);
+
+let firstNumber = prompt("Pick a number");
+let secondNumber = prompt("Pick another number");
+let sum = firstNumber + secondNumber;
+alert(sum);
---
+++ like_button.js
@@ -0,0 +1,3 @@
+const domContainer = document.querySelector('#Like');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));
